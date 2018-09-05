b1c3192157a8d92f3239d784c778a2e796798760
RadicalSquadron <rajesh040.rj@gmail.com>
2018-09-05 13:32:14 +0530

    Initial Commit

---
+++ RSCustomBrowser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RSCustomBrowser
+//
+//  Created by iOS Developer on 05/09/18.
+//  Copyright © 2018 iOS Developer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
